df30ee0822cfbe463d5718bf6c661f59643e806c
Lim Yew Hao <lim_yew_hao@a-star.edu.sg>
2026-07-04 13:59:15 +0800

    feat(python): scaffold vda5050_master_python pybind11 module
    Signed-off-by: Lim Yew Hao <lim_yew_hao@a-star.edu.sg>

---
+++ vda5050_core/python/master_bindings.cpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2026 ROS-Industrial Consortium Asia Pacific
+ * Advanced Remanufacturing and Technology Centre
+ * A*STAR Research Entities (Co. Registration No. 199702110H)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(vda5050_master_python, m)
+{
+  m.doc() = "VDA5050 fleet-master Python bindings";
+
+  m.def_submodule("master", "VDA5050 fleet master API");
+}
